edfea74080a829d2492e188168cf4db7939e5b63
grant smith <grantsmith1256@gmail.com>
2018-11-25 05:59:58 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,10 +12,22 @@ class Transfer
   end
   
   def valid?
-    if @sender.status[3] == @receiver.status[3]
+    if @sender.status == @receiver.status
       true
     else false
     end
   end
+  
+  def execute_transaction
+    counter = 0
+    @sender.balance = @sender.balance - @amount
+    @receiver.balance = @receiver.balance + @amount
+    while counter < 0
+     counter +=1
+  end
+  @status = "complete"
+end
+  
+  
   
 end
