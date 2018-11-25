e1ebc158d630964a445ad0c103ecfbf821db7bee
grant smith <grantsmith1256@gmail.com>
2018-11-25 04:26:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 class BankAccount
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(addition)
+    @balance = @balance + addition
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else false
+    end
+  end
 
 end
