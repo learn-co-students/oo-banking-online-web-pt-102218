a513035c05da7e8144e2c598c71e7a878aa9a8e9
Linda Chang <lindachang186@gmail.com>
2018-11-29 15:21:04 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,4 +11,24 @@ attr_accessor  :sender, :receiver, :status, :amount
    @amount = 50
  end
 
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @sender.balance
+      @receiver.balance
+      @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
