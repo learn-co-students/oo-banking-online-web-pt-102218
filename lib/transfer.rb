63aa1cd70681d9b8d3abd49f3638e83876b5aedc
gsmith77 <43479499+gsmith77@users.noreply.github.com>
2019-01-06 20:42:56 -0600

    Update transfer.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,46 +1,36 @@
 class Transfer
-  
-  
-  attr_accessor :sender, :receiver, :amount, :status
-  
-  def initialize(sender, receiver, amount = 50)
-    @status = "pending"
+
+attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status = "pending", amount)
     @sender = sender
     @receiver = receiver
+    @status = status
     @amount = amount
   end
-  
-  def valid?
-    if @sender.status == @receiver.status
-      true
-    else false
-    end
-    @sender.valid? && @receiver.valid?
-  end
-  
-  def execute_transaction
-    if @sender.balance < @amount
-    @status = "rejected"
-    "Transaction rejected. Please check your account balance."
-    elsif @status == "pending"
-    @sender.balance = @sender.balance - @amount
-    @receiver.balance = @receiver.balance + @amount
-    @status = "complete"
-  else @status == "complete"
-    @status = "complete"
-  end
-end
 
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if sender.balance < @amount
+       @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == "pending"
+      sender.balance = sender.balance - @amount
+      receiver.balance = receiver.balance + @amount
+      @status = "complete"
+    end
+  end
 
   def reverse_transfer
-    if @status = "complete" && @receiver.balance != @sender.balance
-    @receiver.balance = @receiver.balance - @amount
-    @sender.balance = @sender.balance + @amount
-    @receiver.balance == @sender.balance
-    @status = "reversed"
-  else @status = "reversed"
-    @status = "reversed"
+    if @status == "complete"
+      sender.balance = sender.balance + @amount
+      receiver.balance = receiver.balance - @amount
+      @status = "reversed"
+    end
   end
-  end
-  
+
+
 end
